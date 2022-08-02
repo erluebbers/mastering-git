79d32864595423d11ef16c733fef9b1aeb162016
Erik Luebbers <erluebbers@gmail.com>
2022-08-02 14:48:29 -0700

    add fourthFunction and console log

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ function firstFunction() {
   return "Hello World"
 }
 
+console.log("what places do we love")
+
 function secondFunction() {
   return "I love Seattle"
 }
 
 function thirdFunction() {
   return "I love New York"
+}
+
+function fourthFunction() {
+  return "I love Miami"
 }
 
